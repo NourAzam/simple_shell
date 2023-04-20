607dee413cdb761fdc86492af4396dd44c473381
NourAzam <Noureldine.mfathy@gmail.com>
2023-04-19 19:32:06 -0700

    Fixing issues

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(int ac, char **av)
 
 	signal(SIGINT, get_sigint);
 	set_data(&datash, av);
-	loop(&datash);
+	shell_loop(&datash);
 	free_data(&datash);
 	if (datash.status < 0)
 		return (255);
